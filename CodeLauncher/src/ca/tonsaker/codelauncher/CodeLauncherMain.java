bcfe934b82ee62a01b3f999a412e600a920e3611
Markusmoo <markustonsaker@gmail.com>
2015-02-23 00:54:29 -0800

    Added Maven Suppor

--- CodeLauncher/src/ca/tonsaker/codelauncher/CodeLauncherMain.java
+++ CodeLauncher/src/ca/tonsaker/codelauncher/CodeLauncherMain.java
@@ -6,21 +6,46 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
-import javax.swing.JList;
+import javax.swing.JTree;
 
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
 
-public class CodeLauncherMain {
+import ca.tonsaker.codelauncher.ProjectLoader.Project;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+
+public class CodeLauncherMain implements ActionListener,TreeSelectionListener {
 
 	public static String FILE_DIR;
 	
 	private JFrame frmMarkus;
 	private ProjectLoader pLoader;
+	private DefaultMutableTreeNode treeList;
+	JTree tree;
+	
+	private JButton btnRun;
+	
+	private HashMap<String, DefaultMutableTreeNode> projects = new HashMap<String, DefaultMutableTreeNode>();
 
 	/**
 	 * Launch the application.
@@ -57,6 +82,17 @@ public class CodeLauncherMain {
 		}
 		pLoader = new ProjectLoader(FILE_DIR);
 		initialize();
+		createTree();
+	}
+	
+	private void createTree(){
+		ArrayList<Project> jars = pLoader.projects;
+		for(Project jar : jars){
+			this.addProjectNode(jar.getJar().getName());
+			for(File p : jar.getClasses()){
+				this.addNode(jar.getJar().getName(), p.getName());
+			}
+		}
 	}
 
 	/**
@@ -72,17 +108,35 @@ public class CodeLauncherMain {
 		frmMarkus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMarkus.setLocation(100, 100);
 		
-		JList<String> list_1 = new JList<String>(pLoader.getFileNames());
-		//JList<String> list_1 = new JList<String>(); //TODO for appbuilder
+		JPanel panel = new JPanel();
+		panel.setBounds(180, 11, 444, 536);
 		
-		//TODO add syntax textbox https://github.com/bobbylight/RSyntaxTextArea
+		RSyntaxTextArea textArea = new RSyntaxTextArea(33, 56);
+		textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+		textArea.setCodeFoldingEnabled(true);
+		textArea.setEditable(false);
+		textArea.setText("No source available");
+		RTextScrollPane sp = new RTextScrollPane(textArea);
+		panel.add(sp);
 		
-		JScrollPane scroll = new JScrollPane(list_1, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		frmMarkus.getContentPane().add(panel);
+		
+		//JList<String> tree = new JList<String>(pLoader.getJarFileNames());
+		tree = new JTree(); //TODO for appbuilder
+		treeList = new DefaultMutableTreeNode("Projects");
+		tree.addTreeSelectionListener(this);
+		
+		tree.setModel(new DefaultTreeModel(treeList));
+		
+		JScrollPane scroll = new JScrollPane(tree, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scroll.setBounds(10, 11, 160, 570);
-		scroll.getViewport().setView(list_1);
+		scroll.getViewport().setView(tree);
 		frmMarkus.getContentPane().add(scroll);
 		
-		//list_1.
+		btnRun = new JButton("Run");
+		btnRun.setBounds(180, 558, 444, 23);
+		frmMarkus.getContentPane().add(btnRun);
+		btnRun.addActionListener(this);
 		
 		JMenuBar menuBar = new JMenuBar();
 		frmMarkus.setJMenuBar(menuBar);
@@ -115,5 +169,55 @@ public class CodeLauncherMain {
 		mnHelp.add(mntmAbout);
 		
 		frmMarkus.pack();
+	}
+	
+	public void addProjectNode(String name){
+		DefaultMutableTreeNode def = new DefaultMutableTreeNode(name);
+		projects.put(name, def);
+		treeList.add(def);
+	}
+	
+	public void addNode(String project, String name){
+		DefaultMutableTreeNode def = new DefaultMutableTreeNode(name);
+		projects.get(project).add(def);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		if(src.equals(btnRun)){
+			
+		}
+	}
+
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+		
+		if(node == null) return;
+		
+		Project selProject;
+		boolean isJar;
+		File selFile;
+		String sel = node.getUserObject().toString();
+
+		outerloop: for(Project pro : pLoader.projects){
+			if(pro.getJar().getName().equals(sel)){
+				selProject = pro;
+				selFile = pro.getJar();
+				isJar = true;
+				break;
+			}
+			for(File classFile : pro.getClasses()){
+				if(classFile.getName().equals(sel)){
+					selProject = pro;
+					selFile = classFile;
+					isJar = false;
+					break outerloop;
+				}
+			}
+		}
+		
+		//if() //TODO read source files and prep jar for running
 	}
 }
--- CodeLauncher/src/ca/tonsaker/codelauncher/ProjectLoader.java
+++ CodeLauncher/src/ca/tonsaker/codelauncher/ProjectLoader.java
@@ -11,43 +11,128 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
 public class ProjectLoader {
 	
-	public String dir;
-	public ArrayList<File> files = new ArrayList<File>();
-	
-	public ProjectLoader(String dir){
-		this.dir = dir;
-		this.loadFiles();
-		for(String s : getFileNames()) System.out.println("Loaded: "+s);
-	}
-
-	public String[] getFileNames(){
-		String[] fileNames = new String[files.size()];
-		int idx = 0;
-		for(File i : files){
-			fileNames[idx] = i.getName();
-			idx++;
+	public class Project{
+		
+		private String proDir;
+		
+		private boolean valid;
+		
+		private File zip;
+		private File jar;
+		private ArrayList<File> javaClasses;
+		
+		public Project(String zipDir){
+			javaClasses = new ArrayList<File>();
+			proDir = getProjectDir(zipDir);
+			valid = unpackageZip(zipDir, proDir);
+			if(valid) valid = setup();
 		}
-		return fileNames;
-	}
-	
-	public void loadFiles(){
-		File tempFile = new File(dir);
-		if(!tempFile.exists()){
-			return;
-		}else{
-			for(File f : tempFile.listFiles(new FilenameFilter(){
+		
+		protected String getProjectDir(String zip){
+			zip = zip.substring(0, zip.lastIndexOf('.'));
+			new File(zip).mkdirs();
+			return zip;
+		}
+		
+		private boolean setup(){
+			File folder = new File(proDir);
+			for(File f : folder.listFiles(new FilenameFilter(){
 
 				@Override
 				public boolean accept(File e, String s) {
 					return s.toLowerCase().endsWith(".jar");
 				}
 				
+			})) jar = f;
+			
+			for(File f : folder.listFiles(new FilenameFilter(){
+
+				@Override
+				public boolean accept(File e, String s) {
+					return s.toLowerCase().endsWith(".java");
+				}
+				
+			})) javaClasses.add(f);
+			
+			return true;
+		}
+		
+		public boolean unpackageZip(String zipDir, String destDir){
+			try{
+				ZipFile zip = new ZipFile(zipDir);
+				if(!zip.isValidZipFile() || zip.isEncrypted()){
+					throw new IOException("Zip File at "+ zipDir +" is not valid or is encrypted.");
+				}
+				zip.extractAll(destDir);
+			}catch (ZipException e){
+				e.printStackTrace();
+				return false;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+			return true;
+		}
+		
+		public File getJar(){
+			return jar;
+		}
+		
+		public ArrayList<File> getClasses(){
+			return javaClasses;
+		}
+		
+		public File getZip(){
+			return zip;
+		}
+		
+		public boolean isValid(){
+			return valid;
+		}
+		
+	}
+	
+	public String dir;
+	public ArrayList<Project> projects = new ArrayList<Project>();
+	
+	public ProjectLoader(String dir){
+		this.dir = dir;
+		this.loadFiles();
+		for(String s : getJarFileNames()) System.out.println("Loaded: "+s);
+	}
+
+	public String[] getJarFileNames(){
+		String[] fileNames = new String[projects.size()];
+		int idx = 0;
+		for(Project i : projects){
+			fileNames[idx] = i.getJar().getName();
+			idx++;
+		}
+		return fileNames;
+	}
+	
+	public boolean loadFiles(){
+		File tempFile = new File(dir);
+		if(!tempFile.exists()){
+			return false;
+		}else{
+			for(File f : tempFile.listFiles(new FilenameFilter(){
+
+				@Override
+				public boolean accept(File e, String s) {
+					return s.toLowerCase().endsWith(".zip");
+				}
+				
 			})){
-				files.add(f);
+				projects.add(new Project(f.getAbsolutePath()));
 			}
 		}
+		return true;
 	}
 	
 	public static boolean saveDir(String directory) throws IOException{
@@ -78,7 +163,7 @@ public class ProjectLoader {
 		reader.close();
 		file.close();
 		
-		System.out.println("Directory loaded: "+path);
+		System.out.println("Directory loaded: "+loadedDir);
 		return loadedDir;
 	}
 	
